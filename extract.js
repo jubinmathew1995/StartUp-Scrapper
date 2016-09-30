c9544f9be55e02383d77b7c6852618f4eb5b3a26
jubinmathew1995 <jubinmathew1995@gmail.com>
2016-09-30 16:04:20 +0530

    Basic data extraction by the extension.

---
+++ extract.js
@@ -0,0 +1,9 @@
+var list = document.getElementsByTagName('div');
+for (var i = 0; i < list.length; i++) {
+	if(list[i].className == 'show'){
+		console.log(list[i]);
+	}
+	if(list[i].className == 'founders section'){
+		console.log(list[i]);
+	}
+}
