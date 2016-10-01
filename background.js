ddd1b2b6d9d62dfc41e706cdad5f5d51819deea5
jubinmathew1995 <jubinmathew1995@gmail.com>
2016-10-01 11:36:34 +0530

    Extract the revelant data and structure it for further usage.

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 
 chrome.browserAction.onClicked.addListener(function(){
-    chrome.tabs.executeScript(null, {file: 'extract.js'});
-
+	chrome.tabs.executeScript(null, {file: 'jquery.min.js'});
+	chrome.tabs.executeScript(null, {file: 'http://code.jquery.com/jquery-migrate-1.2.1.min.js/jquery.min.js'});
+    chrome.tabs.executeScript(null, {file: 'extract_div.js'});
+    chrome.tabs.executeScript(null, {file: 'extract_name.js'});
+    chrome.tabs.executeScript(null, {file: 'extract_span.js'});
+    chrome.tabs.executeScript(null, {file: 'json_post.js'});
 });
--- extract.js
+++
@@ -1,9 +0,0 @@
-var list = document.getElementsByTagName('div');
-for (var i = 0; i < list.length; i++) {
-	if(list[i].className == 'show'){
-		console.log(list[i]);
-	}
-	if(list[i].className == 'founders section'){
-		console.log(list[i]);
-	}
-}
