169659fe29d1e071f918e55968775ba3638b8080
jubinmathew1995 <jubinmathew1995@gmail.com>
2016-10-01 21:16:17 +0530

    Added CSS and the jQuery POST request for the required data

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var x,m;
+	chrome.storage.sync.get('data', function (data) {
+		x=data.data;
+		//console.log(x);
+		m=x.image.replace('\"',"");
+	document.getElementById('log').src=m;
+	document.getElementById('name').innerHTML=x.title;
+	document.getElementById('desc').innerHTML=x.description;
+	document.getElementById('url').innerText=x.url;
+	document.getElementById('loc').innerHTML=x.location;
+	   $.post("https://beta.crowdproduct.com/api/product",
+        {
+          name: x.title,
+          location: x.location,
+          url:x.url,
+          description:x.description,
+          image:m,
+          source:'angellist'
+        },
+        function(data,status){
+            console.log("Data: " + data.message + "\nStatus: " + status);
+        });
+	});     
+});
